8927a1ab2b300dc2ae177eb5c32fa331adc5e9d1
Moiz K. Malik <moizkhanmalik@gmail.com>
2015-10-18 18:01:11 -0400

    flesh out ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,5 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.id?
+      can :manage, Campaign
+      else
+      can :read, Campaign
+    end
   end
 end
