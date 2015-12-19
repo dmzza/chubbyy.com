4bc5c9989c9082107773942e310335307b5d773a
David Mazza <davidedwardmazza@gmail.com>
2015-12-19 10:40:52 -0500

    let registered users manage everything

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.id?
       can :manage, Campaign
+      can :manage, Pet
+      can :manage, Color
+      can :manage, Shape
+      can :manage, User
       else
       can :read, Campaign
     end
